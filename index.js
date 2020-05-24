e3f4e7cd135cbd249afe037085212c2f718197f9
Julien Furnari <julien.furnari@gmail.com>
2020-05-24 18:31:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(hoiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
